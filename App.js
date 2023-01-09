21d46390ce2ac8400586605aed3b3fda75d3b817
Saqib <iamsaqib24@gmail.com>
2023-01-09 13:28:26 +0530

    day02-done

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 const heading = React.createElement("h1", {className: "heading"}, "Namaste React")
+
+const heading2 = React.createElement("h2", {className: "heading"}, "Igniting our App")
+
+const container = React.createElement("div", {className: "container"}, [heading, heading2])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(container)
