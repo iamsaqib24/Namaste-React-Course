e2b9f1056af8017535c220492e146c828f5a395b
Saqib <iamsaqib24@gmail.com>
2023-01-10 10:48:32 +0530

    destructuring

--- App.js
+++ App.js
@@ -1989,15 +1989,18 @@ const restaurantList = [
       },
 ]
 
-const RestaurantCard = (props) => {
+const RestaurantCard = ({restaurant}) => {
+
+    const {cloudinaryImageId, name, cuisines, avgRating} = restaurant.data
+
     return (
         <div className="card">
             <img src={
                 "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
-                props.restaurant.data?.cloudinaryImageId} />
-            <h2>{props.restaurant.data?.name}</h2>
-            <h3>{props.restaurant.data?.cuisines.join(", ")}</h3>
-            <h4>{props.restaurant.data?.avgRating} stars</h4>
+                cloudinaryImageId} />
+            <h2>{name}</h2>
+            <h3>{cuisines.join(", ")}</h3>
+            <h4>{avgRating} stars</h4>
         </div>
     )
 }
