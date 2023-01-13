6ef294841eb858570fe100f0f063684d79370dff
Saqib <iamsaqib24@gmail.com>
2023-01-13 18:28:48 +0530

    getHooked-done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,41 @@
 import { restaurantList } from '../constants'
 import RestaurantCard from './RestaurantCard'
+import { useState } from 'react'
+
+function filterData(searchInput, restaurants) {
+    return restaurants.filter((restaurant) => restaurant.data.name.includes(searchInput))
+}
 
 const Body = () => {
+    const [restaurants, setRestaurants] = useState(restaurantList)
+    const [searchInput, setSearchInput] = useState("")
+
     return (
-        <div className="retaurant-list">
-            {
-                restaurantList.map(restaurant => <RestaurantCard {...restaurant.data} key={restaurant.data.id} />)
-            }
-        </div>
+        <>
+            <div className="search-container">
+                <input
+                    type="text"
+                    placeholder="search"
+                    className="search-input"
+                    value={searchInput}
+                    onChange={(e) => {
+                        setSearchInput(e.target.value)
+                    }}
+                />
+                <button
+                    className="search-btn"
+                    onClick={() => {
+                        const data = filterData(searchInput, restaurants)
+                        setRestaurants(data)
+                    }}
+                >Search</button>
+            </div>
+            <div className="retaurant-list">
+                {
+                    restaurants.map(restaurant => <RestaurantCard {...restaurant.data} key={restaurant.data.id} />)
+                }
+            </div>
+        </>
     )
 }
 
