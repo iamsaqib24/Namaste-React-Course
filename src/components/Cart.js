09a9099b4e840ead3c0a79fbc480cedf456df1a6
Saqib <iamsaqib24@gmail.com>
2023-02-13 13:51:01 +0530

    added-clear-cart-feature

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { clearCart } from '../utils/cartSlice'
 import FoodItem from './FoodItem'
 
 const Cart = () => {
 
     const cartItems = useSelector(store => store.cart.items)
 
+    const dispatch = useDispatch()
+
+    const handleClearCart = () => {
+        dispatch(clearCart())
+    }
+
   return (
     <div>
         <h1 className="font-bold">Cart Items - {cartItems.length}</h1>
+        <button className="p-2 m-5 bg-fuchsia-200" onClick={() => handleClearCart()}>Clear Cart</button>
         <div className="flex flex-wrap">
             {cartItems.map(item => <FoodItem key={item.id} {...item} />)}
         </div>
