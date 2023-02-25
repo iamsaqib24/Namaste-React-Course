b6b5888c665b25a648ce20d6b8ced6a3207c4415
Saqib <iamsaqib24@gmail.com>
2023-02-25 22:59:44 +0530

    styling-restaurantMenu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
-import { CDN_URL } from "../constants"
+import { CDN_URL, CART_EMPTY } from "../constants"
 import { addItem } from "../utils/cartSlice"
 import useRestaurant from "../utils/useRestaurant"
 import Shimmer from "./Shimmer"
@@ -17,26 +17,48 @@ const RestaurantMenu = () => {
         dispatch(addItem(item))
     }
 
+    console.log(restaurant);
 
   return (!restaurant) ? (<Shimmer />) : (
-    <div className="flex">
-        <div>
-            <h1>Restaurant id : {id}</h1>
-            <h2>{restaurant?.name}</h2>
-            <img src={ CDN_URL + restaurant?.cloudinaryImageId } />
+
+
+
+    <div>
+        <div className="flex bg-gray-900">
+            <img className="h-48 m-5 p-5" src={ CDN_URL + restaurant?.cloudinaryImageId } />
+
+            <div className="flex flex-col justify-evenly">
+                <h2 className="font-bold text-white">{restaurant?.name}</h2>
+                <h3 className="font-bold text-white">{restaurant?.area}</h3>
+                <h3 className="font-bold text-white">{restaurant?.avgRating} stars</h3>
+            </div>
+        </div>
+
+            {/* <h1 className="font-bold text-white">Restaurant id : {id}</h1> */}
             {/* <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ytxglcbv0blbugivcb73" alt="" srcset="" /> */}
-            <h3>{restaurant?.city}</h3>
-            <h3>{restaurant?.avgRating} stars</h3>
+            {/* <h3 className="font-bold text-white">{restaurant?.city}</h3> */}
+
+
+
+
+        <div className="flex justify-center">
+
+            <div className="p-5">
+                <h1 className="font-bold">Menu Lists</h1>
+                <ul>
+                    {Object.values(restaurant?.menu?.items).map((item) => ( <li key={item.id}>{item.name} - <button className="p-2 bg-gray-500 rounded-md text-white" onClick={() => addFoodItem(item)}>add</button></li> ))}
+                </ul>
+            </div>
+
+            <div>
+                <h1 className="text-gray-500 font-bold mb-5 text-3xl">Cart Empty</h1>
+                <img className="h-52" src={CART_EMPTY} alt={"empty cart"} />
+                <h2 className="text-gray-400">Good food is always cooking!</h2>
+                <h2 className="text-gray-400">Go ahead, order some yummy items from the menu.</h2>
+            </div>
+
         </div>
-        {/* <div>
-            <button className="p-2 m-5 bg-purple-500" onClick={() => handleAddItem()}>Add item</button>
-        </div> */}
-        <div className="p-5">
-            <h1 className="font-bold">Menu Lists</h1>
-            <ul>
-                {Object.values(restaurant?.menu?.items).map((item) => ( <li key={item.id}>{item.name} - <button className="p-2 bg-gray-500 rounded-md text-white" onClick={() => addFoodItem(item)}>add</button></li> ))}
-            </ul>
-        </div>
+
     </div>
   )
 }
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
 
+export const CART_EMPTY = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_480/Cart_empty_-_menu_2x_ejjkf2"
+
 export const FETCH_MENU_URL = "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/v4/full?lat=31.3260152&lng=75.57618289999999&menuId="
 
 export const SWIGGY_API = "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.3260152&lng=75.57618289999999&page_type=DESKTOP_WEB_LISTING"
